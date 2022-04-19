6f93dfe122ccf06e326606c432421f11aae0d201
James <James@LAPTOP-O3IJQL37>
2022-04-19 13:31:28 -0400

    ultimo

---
+++ ProyectoFinal/src/logico/Esfera.java
@@ -0,0 +1,17 @@
+package logico;
+
+public class Esfera extends Prisma {
+
+	public Esfera(String id, float alto) {
+		super(id, alto);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2433241166673451783L;
+
+}
+
